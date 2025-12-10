823766f44daccb205e969863b73d0daf4de680de
Jashwanth-13 <24881A0550@student.vardhaman.org>
2025-12-10 22:12:15 +0530

    Create usersMock.js

---
+++ src/mock/usersMock.js
@@ -0,0 +1,4 @@
+export const initialUsers = [
+  { id: "demo-hirer", name: "Demo Hirer", role: "hirer", contact: "" },
+  { id: "demo-worker", name: "Demo Worker", role: "worker", skill: "Helper" }
+];
