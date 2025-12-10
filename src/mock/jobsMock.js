85f877921e8f58d51c27d1a079cc1f09261fd317
Jashwanth-13 <24881A0550@student.vardhaman.org>
2025-12-10 22:11:36 +0530

    Create jobsMock.js

---
+++ src/mock/jobsMock.js
@@ -0,0 +1,14 @@
+export const initialJobs = [
+  {
+    id: "1",
+    title: "House Cleaning",
+    description: "2 hours daily cleaning",
+    location: "Bangalore",
+    pay: "₹500/day",
+    type: "Cleaner",
+    date: "2025-12-12",
+    status: "Open",
+    hirerId: "demo-hirer",
+    assignedWorkerId: null
+  }
+];
